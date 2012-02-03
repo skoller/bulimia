96f06779777255f0f46655ae1d632353a7256999
skoller <nathanskoller@gmail.com>
2012-02-03 10:28:59 -0500

    test remove code

--- app/controllers/phone_controller.rb
+++ app/controllers/phone_controller.rb
@@ -6,40 +6,40 @@ class PhoneController < ApplicationController
     
     number = params['From']
     # session[:ph] = number
-    
-    if session[:day] == true && session[:text_convo] = true
-      if params['Body'] = "t"
-        @pt = Patient.where(:phone_number => session[:ph])
-        @sms_log_entr = @pt.log_entries.build
-        @sms_log_entry.day = 16
-        @sms_log_entry.date = Date.now
-        @sms_log_entry.save(:validate => :false)
-        session[:day] = :false
-        return false
-      elsif params['Body'] = "x"
-        @pateint = Patient.where(:phone_number => session[:ph])
-      else
-        @error_day = true
-        render BASE_DIR + "day.xml"
-        return false
-      end
-    end
-    
-    if session[:time] == true && session[:text_convo] = true
-      #####
-    end
-    
-    if session[:food] == true && session[:text_convo] = true
-      #####
-    end
-    
-    if session[:bvl] == true && session[:text_convo] = true
-      #####
-    end
-    
-    if session[:note] == true && session[:text_convo] = true
-      #####
-    end
+        # 
+        # if session[:day] == true && session[:text_convo] = true
+        #   if params['Body'] = "t"
+        #     @pt = Patient.where(:phone_number => session[:ph])
+        #     @sms_log_entr = @pt.log_entries.build
+        #     @sms_log_entry.day = 16
+        #     @sms_log_entry.date = Date.now
+        #     @sms_log_entry.save(:validate => :false)
+        #     session[:day] = :false
+        #     return false
+        #   elsif params['Body'] = "x"
+        #     @pateint = Patient.where(:phone_number => session[:ph])
+        #   else
+        #     @error_day = true
+        #     render BASE_DIR + "day.xml"
+        #     return false
+        #   end
+        # end
+        # 
+        # if session[:time] == true && session[:text_convo] = true
+        #   #####
+        # end
+        # 
+        # if session[:food] == true && session[:text_convo] = true
+        #   #####
+        # end
+        # 
+        # if session[:bvl] == true && session[:text_convo] = true
+        #   #####
+        # end
+        # 
+        # if session[:note] == true && session[:text_convo] = true
+        #   #####
+        # end
     
     ######################## First incoming text #############################
     
