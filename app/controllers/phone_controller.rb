c3d6cf02a6f4a2e9abafbc0fb1a40456ae2025c8
skoller <nathanskoller@gmail.com>
2012-02-03 23:52:06 -0500

    good stuff update

--- app/controllers/phone_controller.rb
+++ app/controllers/phone_controller.rb
@@ -3,43 +3,45 @@ class PhoneController < ApplicationController
   BASE_DIR = "phone/" 
   
   def convo_handler_state_changer
-        raw_number = params['From']
-        number_mod = raw_number.tr('+-/)/(', '')
+    raw_number = params['From']
+    number_mod = raw_number.tr('+-/)/(', '')
 
-        if ( number_mod =~ /^1\d(10)/ ) || ( number_mod =~ /^\d(10)/ )
-          if number_mod.size == 11
-            @processed_num = number_mod.slice!(1..10)
-          elsif number_mod.size == 10
-            @processed_num = number_mod.slice!(0..9)
-          end
-        else
-          render BASE_DIR + "number_problem.xml"
-          return false
-        end
-    
-    if  session[:bla]
-      
-      
+    if ( number_mod =~ /^1\d(10)/ ) || ( number_mod =~ /^\d(10)/ )
+      if number_mod.size == 11
+        @processed_num = number_mod.slice!(1..10)
+      elsif number_mod.size == 10
+        @processed_num = number_mod.slice!(0..9)
+      end
     else
-      
-        
-      
-      @ch = Convo_handler.build
-      @ch.patient_id = Patient.where(:phone_number => @processed_num).first.id
-      @ch.state = 'start'
-      @ch.save(:validate => :false)
-      @le = Log_entry.build
-      @le.patient.id = Patient.where(:phone_number => @processed_num).first.id
-      @le.food = "THIS IS A TEST"
-      @le.save(:validate => :false)
-      @ch.log_entry_id = @le.id
-      @ch.save(:validate => :false)
-      
+      render BASE_DIR + "number_problem.xml"
+      return false
+    end
+
+    if  session[:bla]
+
+
+    else
+
+      if @patient = Patient.where(:phone_number => @processed_num).first
+        @ch = ConvoHandler.build
+        @ch.patient_id = @patient.id
+        @ch.state = 'start'
+        @ch.save(:validate => :false)
+        @le = LogEntry.build
+        @le.patient_id = @patient.id
+        @le.food = "THIS IS A TEST"
+        @le.save(:validate => :false)
+        @ch.log_entry_id = @le.id
+        @ch.save(:validate => :false)
+      else
+        render BASE_DIR + "number_problem.xml"
+        return false
+      end
       render BASE_DIR + "cool.xml"
     end
-    
+
   end
-  
+
   
   
   
