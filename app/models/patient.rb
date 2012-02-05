5cf9eeb6ce6d960bf9e9795823986002f1a86767
David Gershuni <vidgersh@gmail.com>
2012-02-04 16:28:29 -0800

    day index / base date

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,11 +4,11 @@ class Patient < ActiveRecord::Base
 
   def base_date
   	unless @base_date
-    	@base_date = @patient.log_entries.first.created_at.change(:hour => 8)
+    	@base_date = self.log_entries.first.created_at.change(:hour => 8)
   	end
 	end
 
 	def determine_log_entry_day_index(log_entry)
-  	return (self.base_date - log_entry.created_at.change(:hour => 8)).to_i
+  	return (self.base_date - log_entry.date.change(:hour => 8)).to_i + 1
 	end
 end
