7e33ed0a3f4252044188b614ba4bfee443c9723a
David Gershuni <vidgersh@gmail.com>
2012-02-04 16:22:00 -0800

    day index

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,14 @@
 class Patient < ActiveRecord::Base
   has_many :log_entries
   has_one :convo_handler
+
+  def base_date
+  	unless @base_date
+    	@base_date = @patient.log_entries.first.created_at.change(:hour => 8)
+  	end
+	end
+
+	def determine_log_entry_day_index(log_entry)
+  	return (self.base_date - log_entry.created_at.change(:hour => 8)).to_i
+	end
 end
