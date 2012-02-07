450ec7516549ac4fbf19c879b0efef84e3d991c3
skoller <nathanskoller@gmail.com>
2012-02-06 19:24:39 -0500

    fixed pdf columns to fixed widths

--- app/pdfs/log_pdf.rb
+++ app/pdfs/log_pdf.rb
@@ -1,10 +1,11 @@
 class LogPdf < Prawn::Document
   
     def initialize(patient)
-      super(top_margin: 50)
+      super(top_margin: 40)
         @patient = patient
         patient_info 
         entries
+       
     end
     
     def patient_info
@@ -14,16 +15,27 @@ class LogPdf < Prawn::Document
     
     def entries
         move_down 20
+        
         table entry_rows do
+          
           row(0).font_style = :bold
-          columns(1..3).align = :right
+          row(0).align = :center
           self.row_colors = ["DDDDDD", "FFFFFF"]
           self.header = true
+          column(0).width = 35
+          column(1).width = 55
+          column(0..1).align = :center
+          column(2).width = 90
+          column(3).width = 100
+          column(2..3).align = :left
+          column(4..6).width = 45
+          column(7).width = 125
+          column(7).align = :left
         end
       end
-
+      
       def entry_rows
-        [["Day", "Time", "Where", "Food", "Binge", "Vomit", "Laxative", "Personal Notes"]] +
+        [["Day", "Time", "Where", "Food", "Binge", "Vomit", "Lax", "Personal Notes"]] +
         @patient.log_entries.map do |entry|
           [entry.day, entry.time, entry.location, entry.food, b_v_l(entry.binge), b_v_l(entry.vomit), b_v_l(entry.laxative), entry.personal_notes]
         end
