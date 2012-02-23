3dd52f79023997c8eb485a0ccadfd1dafbf04f49
skoller <nathanskoller@gmail.com>
2012-02-22 22:39:17 -0500

    fixed security issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,73 +5,74 @@ class SessionsController < ApplicationController
   end
 
   def new_patient_session
-    session[:patient_id] = nil 
+    session[:patient_id] = nil
+    session[:first_login_restriction] = true 
   end
 
   def create_ph_session
     ph = Physician.where(:email => (params[:email])).first
     unless ph == nil
-     if (ph.email == 'dev@bvl.com') && ph.authenticate(params[:password])
-      session[:physician_id] = 1
-      redirect_to admin_path(ph)
-     elsif (ph.email != 'dev@bvl.com') && ph.authenticate(params[:password]) && ph.state
-      session[:physician_id] = ph.id
-      redirect_to physician_patients_path(ph)
-     elsif (ph.email != 'dev@bvl.com') && ph.authenticate(params[:password]) && ph.state == nil
-      session[:physician_id] = ph.id
-      redirect_to physician_additional_info_path(:id => ph.id)
-    elsif !ph.authenticate(params[:password])
-      render "new_physician_session" 
-     end
+      if (ph.email == 'dev@bvl.com') && ph.authenticate(params[:password])
+        session[:physician_id] = 1
+        redirect_to admin_path(ph)
+      elsif (ph.email != 'dev@bvl.com') && ph.authenticate(params[:password]) && ph.state
+        session[:physician_id] = ph.id
+        redirect_to physician_patients_path(ph)
+      elsif (ph.email != 'dev@bvl.com') && ph.authenticate(params[:password]) && ph.state == nil
+        session[:physician_id] = ph.id
+        redirect_to physician_additional_info_path(:id => ph.id)
+      elsif !ph.authenticate(params[:password])
+        render "new_physician_session" 
+      end
     else
       render "new_physician_session"
     end
   end
-  
+
   def new_patient_start_code_entry
   end
-  
+
   def new_patient_code_verification
     pt_phone_number = Patient.where(:phone_number => params[:phone_number]).first
     pt_code = Patient.where(:start_code => params[:start_code]).first
     unless pt_phone_number == nil || pt_code == nil
-       if pt_phone_number.signup_status = "returning"
-         render 'new_patient_session'
-       end
-        if pt_code.phone_number == pt_phone_number.phone_number
-          session[:start] = pt_code.id
-          redirect_to new_patient_password_setup_path(:pt_id => pt_code.id)
-        elsif pt_code.phone_number && !pt_phone_number.phone_number
-          render 'new_patient_start_code_entry', flash.now.alert = "Bivola does not recognize the start code you provided."
-        elsif !pt_code.phone_number && pt_phone_number.phone_number
-          render 'new_patient_start_code_entry', flash.now.alert = "Bivola does not recognize the phone number you provided."  
-        end
-      else
-          render 'new_patient_start_code_entry', flash.now.alert = "Bivola does not recognize the phone number or start code you provided."  
+      if pt_phone_number.signup_status = "returning"
+        render 'new_patient_session'
       end
-      
+      if pt_code.phone_number == pt_phone_number.phone_number
+        session[:start] = pt_code.id
+        redirect_to new_patient_password_setup_path(:pt_id => pt_code.id)
+      elsif pt_code.phone_number && !pt_phone_number.phone_number
+        render 'new_patient_start_code_entry', flash.now.alert = "Bivola does not recognize the start code you provided."
+      elsif !pt_code.phone_number && pt_phone_number.phone_number
+        render 'new_patient_start_code_entry', flash.now.alert = "Bivola does not recognize the phone number you provided."  
+      end
+    else
+      render 'new_patient_start_code_entry', flash.now.alert = "Bivola does not recognize the phone number or start code you provided."  
+    end
+
   end
-  
+
   def new_patient_password_setup
     @patient = Patient.find(params[:pt_id])
   end
-  
+
   def password_set
     @patient = Patient.find(params[:pt_id])
     if (session[:start]).to_s == @patient.id.to_s
-    ############ CURRENTLY @patient evals to nil
+      ############ CURRENTLY @patient evals to nil
       if @patient.update_attributes(params[:patient])
         session[:start] = nil
         redirect_to patient_matched_path(:phone_number => @patient.phone_number)
       else
         redirect_to new_patient_password_setup_path(:pt_id => @patient.id)
       end
-      
+
     else
       redirect_to home_page_path
     end
   end
-    
+
 
   def create_pt_session
     pt = Patient.where(:phone_number => params[:phone_number]).first
@@ -89,16 +90,20 @@ class SessionsController < ApplicationController
           render "new_patient_session", :notice => "Invalid email or password"
         end
       elsif pt.signup_status == "new"
-        session[:patient_id] = pt.id
-        pt.signup_status = "returning"
-        pt.save(:validate => false)
-        session[:start] = nil
-        redirect_to patient_welcome_path(:patient_id => pt.id)
+        if session[:first_login_restriction]
+          redirect_to new_patient_start_code_entry_path, :notice => "Because you are a new patient, please sign up here." 
+        else
+          session[:patient_id] = pt.id
+          pt.signup_status = "returning"
+          pt.save(:validate => false)
+          session[:start] = nil
+          redirect_to patient_welcome_path(:patient_id => pt.id)
+        end
       else
         redirect_to home_page_path
       end
     else
-        redirect_to home_page_path
+      redirect_to home_page_path
     end
   end
 
