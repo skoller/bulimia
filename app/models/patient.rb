60cefdf13c4b76465f2ca2472bcfb195cee5537e
Nathan Skoller <nathanskoller@Nathans-MacBook-Pro.local>
2012-11-23 20:33:45 -0500

    fully functional doctor and admin

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -11,7 +11,7 @@ class Patient < ActiveRecord::Base
   validates_presence_of :phone_number, :format => { :with => /^\d{10}$/, :message => "Must be 10 digits and contain no parathesis or dashes" }
   validates_uniqueness_of :phone_number
   validates_presence_of :sex, :on => :create
-  validates_presence_of :dob, :on => :create, :format => { :with => /^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d$/, :message => "MM-DD-YYYY format required" }
+  validates_presence_of :dob, :on => :create, :format => { :with => /^(0[1-9]{1}|[12]{1}[0-9]{1}|3[01]{1}).{1}(0[1-9]{1}|1[0-2]{1}).{1}([12]{1}[0-9]{3})$/, :message => "MM-DD-YYYY format required" }
   validates_presence_of :diagnosis, :on => :create
   
   has_many :log_entries
